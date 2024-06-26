84e20c0eccf90edf5533475cb79200c2e267422e
umar2111k <138141263+umar2111k@users.noreply.github.com>
2024-06-26 12:05:45 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "23475322"))
+  API_HASH = os.environ.get("API_HASH", "e00e5cebf073df8baba7db34ea0ebdc9")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7254390943:AAGr45HMuJqGCDrLobX4HTi50Z_FLtcUegI")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "File_store_56bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-102197836805"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "adrinolinks.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "80118c72026ffc6debd8d4d909af5bf753a86f4a")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6170050819"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Umarxofficiall:cappuccino6005205125.@cluster0.uwzmp5o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002187391758")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002246184289"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -32,11 +32,10 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [Umar](https://t.me/+3zQFUNujkupiZmM1)
  
- I am Super noob Please Support My Hard Work.
+  Support My Hard Work Youtuber king.
 
-[Donate Me](https://t.me/KingVj01)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
